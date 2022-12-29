17491c9a9617ddebfc624dd5f2fdd2dc593d32cd
nadintka <nadintka@mail.ru>
2022-12-29 13:25:22 +0300

    add print

---
+++ lesson 3. 2.py
@@ -0,0 +1,5 @@
+number1 = int(input('Введите первое число'))
+number2 = int(input('Введите второечисло'))
+number3 = int(input('Введите третье число'))
+number4 = (number1+number2+number3)/3
+print(number4)
